43fca3fd66789d19994455875093ef2401d1e156
Neema <neemapriscillar21@gmail.com>
2018-01-05 14:02:26 +0300

    Receipt template

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rentkeny_realestate',
     'username' => 'root',
-    'password' => 'Ict@Dev17**',
+    'password' => '',
 	'tablePrefix' => 're_',
     'charset' => 'utf8',
 ];
