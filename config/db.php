b6acda7d22b1b566910c6ee20e35aab2b4e1041c
Neema <neemapriscillar21@gmail.com>
2018-01-10 09:19:01 +0300

    updated finance layout

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=real_estate2',
     'username' => 'root',
-    'password' => 'pass',
+    'password' => '',
 	'tablePrefix' => 're_',
     'charset' => 'utf8',
 ];
